266d5056478448541a65c03c13bfe340b8805e58
Nick Inman <nick@theoatmeal.com>
2018-11-18 22:49:16 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(word)
   wordarray = word.split(" ")
   array = []
-  wordarray.each do |word| warray << word.reverse
+  wordarray.each do |word| wordarray << word.reverse
 end
 array.join(" ")
 end
